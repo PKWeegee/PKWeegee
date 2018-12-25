753aa01a272e884c19d49c231b714649db1b1163
PKWeegee <pk.weegee@gmx.at>
2018-12-25 04:20:45 +0100

    initial commit

---
+++ CirnAya/patch.js
@@ -0,0 +1,11 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/base_tasofro"
+	],
+	"id": "CirnAya",
+	"servers": [
+		"_/CirnAya/"
+	],
+	"title": "CirnAya"
+}
---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+	"contact": "PKWeegee#7899",
+	"id": "PKWeegee",
+	"patches": {
+		"CirnAya": "Playable Cirno and Aya"
+	},
+	"servers": [
+		"_"
+	],
+	"title": "PKWeegee's patches"
+}
