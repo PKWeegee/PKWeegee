6a030797b6d7d1be002a2bae209a48cc329ddbb2
PKWeegee <pk.weegee@gmx.at>
2019-04-07 10:38:53 +0200

    updated repo.jk

--- repo.js
+++ repo.js
@@ -5,10 +5,7 @@
 		"CirnAya": "CirnAya"
 	},
 	"servers": [
-		"https://thcrap.nmlgc.net/repos/PKWeegee/",
-		"http://thcrap.nmlgc.net/repos/PKWeegee/",
 		"https://mirrors.thpatch.net/PKWeegee/",
-		"http://mirrors.thpatch.net/PKWeegee/"
 	],
 	"title": "PKWeegee's patches"
 }
