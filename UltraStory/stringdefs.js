27e79388bd82ba85ef0bedc67167ec7e6ead251c
PKWeegee <pk.weegee@gmx.at>
2019-06-15 15:51:40 +0200

    UltraStory Th07 Demo

--- UltraStory/stringdefs.js
+++ UltraStory/stringdefs.js
@@ -1,4 +1,5 @@
 {
 	"th165": "Touhou Dream Rebellion Episode 1 - The Anger of the Dream Dwellers",
-	"th06": "Touhou Dream Rebellion Episode 2 - Returning Scarlet Devil"
+	"th06": "Touhou Dream Rebellion Episode 2 - Returning Scarlet Devil",
+	"th07": "Touhou Dream Rebellion Episode 3 - Eternal Winter Storm"
 }
---
+++ UltraStory/th07/musiccmt.js
@@ -0,0 +1,158 @@
+{
+    "1": [
+        "@",
+        "This is the title screen theme.",
+        "Alrighty, next game, new stuff to try.",
+        "Going into this, I'll go on a hunch,",
+        "and say that the supernatural border will be very",
+        "important for survival this time around. Let's see what happens."
+    ],
+    "2": [
+        "@",
+        "This is the Stage 1 theme.",
+        "When I first saw Cirno's non-spell, I got scared.",
+        "The bullet limit is so much higher than in EoSD...",
+        "This will complicate things in terms of balancing, I feel.",
+        "But it should still be fine...?"
+    ],
+    "3": [
+        "@",
+        "This is Letty Whiterock's theme.",
+        "Stage 1 is done, nice.",
+        "I'm trying a different approach as to how I'm handling progress,",
+        "cause I want to finish one stage completely before going to the next one.",
+        "Should give me a better sense of completion."
+    ],
+    "4": [
+        "@",
+        "This is the Stage 2 theme.",
+        "Cats.",
+        "I like cats, had three when I was a little kid.",
+        "But I also know how much they can scratch at times.",
+        "Oh uh...right, gameplay.",
+        "This stage got quite chaotic as well, im getting a tad scared."
+    ],
+    "5": [
+        "@",
+        "This is Chen's theme.",
+        "The whole mystery of Chen has faded ever since the original PCB.",
+        "We all know now that she's a shikigami of a shikigami.",
+        "So at the very least, I can poke fun at that now.",
+        "Idk if I should though, cats can be jerks."
+    ],
+    "6": [
+        "@",
+        "This is the Stage 3 theme.",
+        "This stage is weird.",
+        "Like, why does Alice show up twice as a midboss?",
+        "Oh well, at least it's short in general.",
+        "I glanced at the ecl for St4 and...help."
+    ],
+    "7": [
+        "@",
+        "This is Alice Margatroid's theme.",
+        "End of the demo.",
+        "Yeah, I'll go and release the first three stages early,",
+        "so it won't be a long wait until new stuff is out.",
+        "Maybe I'll release extra seperately as well...",
+        "Who knows."
+    ],
+    "8": [
+        "@",
+        "This is the Stage 4 theme. The style's more Chinese than Japanese. A wonderful place",
+        "above the clouds. As the true beginning of this game, Stage 4 is far longer than the others.",
+        "There's never been such a stratospheric (it's not that high) intrusion",
+        "with violent yet sorrowful music, has there? While I composed it with \"Journey",
+        "to the West\" in mind, it doesn't resemble that."
+    ],
+    "9": [
+        "@",
+        "This is the three Prismriver Sisters' theme.",
+        "I tried to make this theme sound like it was being performed brightly and joyfully.",
+        "A performance by happy-go-lucky sisters who love festivals and anything noisy.",
+        "At the climax, each of them play their violin, trumpet, and keyboard as",
+        "they please. A veritable hodgepodge."
+    ],
+    "10": [
+        "@",
+        "This is the Stage 5 theme.",
+        "This theme expresses the nostalgia that's deep within every Japanese and the",
+        "frantic feeling that arises when someone's approaching.",
+        "The sounds of far-off Shinto flutes and bells conjure up images of the Netherworld.",
+        "Or maybe it's just me?"
+    ],
+    "11": [
+        "@",
+        "This is Youmu Konpaku's theme. I tried to express both \"A Tough Opponent\" and",
+        "\"Childishness\" with this theme. Perhaps this is the most boss-like theme in this game.",
+        "While it uses guitars, I gave up trying to make it sound like rock music almost",
+        "immediately (laugh). The hook's melody resembles the youkai."
+    ],
+    "12": [
+        "@",
+        "This is the Final Stage theme.",
+        "The Final Stage is all about the final boss, so the mid-stage is only a token effort.",
+        "Because of that, this song's rather short, but it's also refreshing and pleasant (maybe).",
+        "The stage has visual imagery of flying over a sea of cherry blossoms;",
+        "I hope this song will give you the same impression."
+    ],
+    "13": [
+        "@",
+        "This is Yuyuko Saigyouji's theme.",
+        "I gave this theme an oriental and melodious feeling while making it the \"final\" song.",
+        "Having said that, the style of music is my usual fare. (laugh)",
+        "Vehement yet elegant, powerful but ephemeral; I thought I'd make this theme",
+        "quite emotional."
+    ],
+    "14": [
+        "@",
+        "This is the ??? theme.",
+        "An arranged version of Yuyuko's theme.",
+        "I put more emphasis on the mysterious atmosphere in this one.",
+        "However, the melody itself is so harsh that it's almost tiresome. (laugh)"
+    ],
+    "15": [
+        "@",
+        "This is the Extra Stage theme.",
+        "A high-paced theme (?) that's unusual for Touhou.",
+        "It's obvious from the stage that the tempo and feel are the keys to this song.",
+        "The tone turns around about 112 degrees in the middle of the song,",
+        "which is where Extra really starts. (laugh)"
+    ],
+    "16": [
+        "@",
+        "This is Ran Yakumo's theme.",
+        "I composed it to be more dazzling than fantastic.",
+        "Hopefully, you'll get the sense of the energetic youkai from the song.",
+        "In my opinion, when there's a long fight in a shooting game, the music should",
+        "repeat and sustain its tempo. That is, it ought to be like this theme."
+    ],
+    "17": [
+        "@",
+        "This is the ??? theme.",
+        "Another version of \"Youkai Domination.\"",
+        "It's not that different to the original though..."
+    ],
+    "18": [
+        "@",
+        "This is Yukari Yakumo's theme.",
+        "Meant to be an arranged version of Ran's theme, it ended up quite different.",
+        "This one has an ominous feeling. Or rather, she's the very embodiment",
+        "of ominous. Even compared with the past characters, she has a devious",
+        "appearance, a dubious character, and thoughtless danmaku.",
+        "The theme's just like her: devious. (laugh)"
+    ],
+    "19": [
+        "@",
+        "This is the ending theme.",
+        "...It's just, it feels kind of dark.",
+        "",
+        "It's supposed to represent a peaceful, everyday life, I swear."
+    ],
+    "20": [
+        "@",
+        "This is the credits theme.",
+        "The imagery is based on the familiar song \"Sakura, Sakura.\"",
+        "I think this song is kind of refreshing."
+    ]
+}
