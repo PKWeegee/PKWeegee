6d5d6fbe329c0cdc8d079a570e671433819077e0
PKWeegee <pk.weegee@gmx.at>
2019-12-19 22:11:32 +0100

    UltraStory Th08 Demo

--- UltraStory/stringdefs.js
+++ UltraStory/stringdefs.js
@@ -1,5 +1,6 @@
 {
 	"th165": "Touhou Dream Rebellion Episode 1 - The Anger of the Dream Dwellers",
 	"th06": "Touhou Dream Rebellion Episode 2 - Returning Scarlet Devil",
-	"th07": "Touhou Dream Rebellion Episode 3 - Eternal Winter Storm"
+	"th07": "Touhou Dream Rebellion Episode 3 - Eternal Winter Storm",
+	"th08": "Touhou Dream Rebellion Episode 4 - Eternal Night of Dreams"
 }
---
+++ UltraStory/th08/musiccmt.js
@@ -0,0 +1,184 @@
+{
+    "1": [
+        "@",
+        "This is the title screen theme.",
+        "I'm back, bitches.",
+        "So uh, this game will be even more challenging for me.",
+        "Why? The ecl is completely different from 7's...why ZUN?",
+        "This means I can't give you extra life drops...sorry."
+    ],
+    "2": [
+        "@",
+        "This is the Stage 1 theme.",
+        "Fireflies are everywhere!",
+        "Yep, I'm trying to one-up myself again.",
+        "The only part I haven't touched yet were the stage looks themselfes.",
+        "So I guess it's time to show how the incident affects known areas.",
+        "Who knows, they might change more drastically later~"
+    ],
+    "3": [
+        "@",
+        "This is Wriggle Nightbug's theme.",
+        "Wriggle's spells have turned into a mess.",
+        "Once again, I apologize for not giving more resources,",
+        "but ZUN was being a shitstick :V",
+        "",
+        ""
+    ],
+    "4": [
+        "@",
+        "This is the Stage 2 theme.",
+        "With the bugs chased away",
+        "we lost the only source of light.",
+        "Many youkai now lurk in the shadows, ready to attack",
+        "unsuspecting humans.",
+        ""
+    ],
+    "5": [
+        "@",
+        "This is Mystia Lorelei's theme.",
+        "Not sure what I did, but some of her bullets turn into stars?",
+        "Gives it a kind of cool asthetic, so I'm not to keen on",
+        "trying to fix that.",
+        ""
+    ],
+    "6": [
+        "@",
+        "This is the Stage 3 theme.",
+        "I didn't have much room with how I could change this stage,",
+        "but it's abstract nature worked well for a dream space.",
+        "Really makes you wonder what the point of it is.",
+        "",
+        "Probably just to mess with you."
+    ],
+    "7": [
+        "@",
+        "This is Keine Kamishirasawa's theme.",
+        "She's not that strong without the moon.",
+        "Even if the screenfilling mess of bullets would like",
+        "to argue differently.",
+        "Either way, this will be the end of the demo.",
+        "Let's meet back here for the rest!"
+    ],
+    "8": [
+        "@",
+        "This is the Stage 4 theme.",
+        "This song's an arrangement of the game's title screen theme.",
+        "It's a nice tune with a very quick tempo.",
+        "For the end, I put in a laughing sound, like that of a youkai.",
+        "However, it's unnatural to hear it where it shouldn't be creepy.",
+        "Altogether, the rhythm is odd. I feel there's a hint of encroaching madness in it."
+    ],
+    "9": [
+        "@",
+        "This is Reimu Hakurei's theme.",
+        "If you're wondering why this theme is nostalgic, it's an arrangement of a song from a few years ago.",
+        "It's an arranged version of Reimu's Stage 4 boss theme from the fourth game, Lotus Land Story.",
+        "The patterns in Stage 4 closely resembles those of Lotus Land Story,",
+        "so I thought I'd do a similar song and give some people déjà vu.",
+        "The other theme for this game is \"An Old Memory Called Fantasy.\""
+    ],
+    "10": [
+        "@",
+        "This is Marisa Kirisame's theme.",
+        "At this point, this song isn't all nostalgic or anything. It's an arrange of an old",
+        "tune from the second game, Story of Eastern Wonderland.",
+        "I was wondering if this theme still suited Marisa, and what shocked me is",
+        "that it totally did. It's rare to see someone so unchanged.",
+        "Marisa has 5 themes, but I think this one suits her best."
+    ],
+    "11": [
+        "@",
+        "This is the Stage 5 theme.",
+        "Asian, Western, old, new; this song isn't one or the other.",
+        "The boss seems that way, too, but the stage itself is based in Japanese horror.",
+        "Each song has a theme, and this one's is based on the song \"Kagome, Kagome.\"",
+        "It's not an arrangement, just composed with a similar theme.",
+        "I didn't want to get caged in on a theme for this one."
+    ],
+    "12": [
+        "@",
+        "This is Reisen Udongein Inaba's theme.",
+        "When you think of a boss in the final stages of Touhou,",
+        "this is what comes to mind. Guess some things never change. (laugh)",
+        "After the wild intro, there's a mournful melody in the middle before the hook breaks through.",
+        "Ah, all the characters look like they're having so much fun."
+    ],
+    "13": [
+        "@",
+        "This is the Stage 6 theme.",
+        "The 20th century voyagers.",
+        "The Noah's Ark of the 20th century flew into space, carrying with it hopes and fears.",
+        "However, it seems that just its hopes were left behind on the Moon.",
+        "Here in the \"future,\" in the 21st century, only fears and a handful",
+        "of illusions seem to be all that remain."
+    ],
+    "14": [
+        "@",
+        "This is Eirin Yagokoro's theme.",
+        "Impossible courage, ferocity, and vigor.",
+        "This theme combines all of those, making for a ridiculously foolish and energetic song.",
+        "All the energy and foolishness culminates in the final boss (ehh?),",
+        "so the act itself of putting this song in a shmup is lunacy.",
+        "I didn't want to get caged in on a theme for this one."
+    ],
+    "15": [
+        "@",
+        "This is Kaguya Houraisan's theme.",
+        "Boy, she really is insane, huh? (laugh) This song brings out a feeling of losing one's cool.",
+        "I don't express emotions through songs, I make songs with emotions.",
+        "And really, the game makes the songs.",
+        "They're not very cool and modern, are they? (Haha)",
+        "I'll admit I'm no pro."
+    ],
+    "16": [
+        "@",
+        "This is the Last Spell theme.",
+        "All it inspires is a frantic feeling. It's seriously fast.",
+        "It's the constant repetition of a short phrase that makes it so fast.",
+        "That doesn't sound like my kind of morning. (laugh)",
+        "Of course, it is fast, but it also has a mysterious kind of nostalgia."
+    ],
+    "17": [
+        "@",
+        "This is the Extra Stage theme.",
+        "It's kind of relaxed for one, huh?",
+        "It feels like a test of courage that wouldn't scare anybody. (No, it doesn't.)",
+        "I'm actually a little unsure about it, but it's extended.",
+        "I collected E, X, T, E, N, and D and extended it."
+    ],
+    "18": [
+        "@",
+        "This is Fujiwara no Mokou's theme.",
+        "",
+        "I tried to bring out as much intimidation, power, and coolness I could that some shadiness",
+        "came out. But even if the song isn't shady, the character certainly is.",
+        "",
+        "I always like kicking it old school for the Extra Boss song. This time around I really kicked it."
+    ],
+    "19": [
+        "@",
+        "This is the ending theme.",
+        "It's still the same. All laid back.",
+        "The ending song is never all that upbeat.",
+        "And this one's no exception."
+    ],
+    "20": [
+        "@",
+        "This is the credits theme.",
+        "",
+        "I added a little nostalgia to this one.",
+        "\"Maple,\" such an old memory.",
+        "",
+        "This theme is loaded with old memories. But there's far too few staff."
+    ],
+    "21": [
+        "@",
+        "This is the Last Word theme.",
+        "I wanted it to invoke feelings that deviate from the frame of the story.",
+        "I'm not trying for coolness.",
+        "I'm not trying for cuteness.",
+        "I'm not even trying for creepiness.",
+        "But it'd be nice if it doesn't put you in a bad mood after listening to it for so long."
+    ]
+}
