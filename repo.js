e15f188135a2d866eb8adc2753d410214814f1da
PKWeegee <pk.weegee@gmx.at>
2020-01-21 20:45:02 +0100

    repo.js update

--- repo.js
+++ repo.js
@@ -2,8 +2,8 @@
 	"contact": "PKWeegee#7899",
 	"id": "PKWeegee",
 	"patches": {
-		"CirnAya": "CirnAya",
-		"UltraStory": "UltraStory"
+		"CirnAya": "Playable Cirno and Aya",
+		"UltraStory": "Harder Patterns, New Story, House Set Music Remixes"
 	},
 	"servers": [
 		"https://mirrors.thpatch.net/PKWeegee/"
