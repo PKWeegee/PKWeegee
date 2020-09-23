b0ceccfddbfe79b2027e13da1470a5e5b3cef0a7
PKWeegee <pk.weegee@gmx.at>
2020-09-23 23:55:01 +0200

    UltraStory th09 Batch1

--- UltraStory/stringdefs.js
+++ UltraStory/stringdefs.js
@@ -2,5 +2,6 @@
 	"th165": "Touhou Dream Rebellion Episode 1 - The Anger of the Dream Dwellers",
 	"th06": "Touhou Dream Rebellion Episode 2 - Returning Scarlet Devil",
 	"th07": "Touhou Dream Rebellion Episode 3 - Eternal Winter Storm",
-	"th08": "Touhou Dream Rebellion Episode 4 - Eternal Night of Dreams"
+	"th08": "Touhou Dream Rebellion Episode 4 - Eternal Night of Dreams",
+	"th09": "Touhou Dream Rebellion Episode 5 - Phantasmagoria of Harvest Abundance"
 }
