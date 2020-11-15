0a817a065bcaa059fd216bcd75b1c214c56e1b27
PKWeegee <pk.weegee@gmx.at>
2020-11-15 17:21:33 +0100

    UltraStory th10 Demo

--- UltraStory/stringdefs.js
+++ UltraStory/stringdefs.js
@@ -4,5 +4,6 @@
 	"th07": "Touhou Dream Rebellion Episode 3 - Eternal Winter Storm",
 	"th08": "Touhou Dream Rebellion Episode 4 - Eternal Night of Dreams",
 	"th09": "Touhou Dream Rebellion Episode 5 - Phantasmagoria of Harvest Abundance",
-	"th095": "Touhou Dream Rebellion Episode 5.5 - Dreamful Chronicles"
+	"th095": "Touhou Dream Rebellion Episode 5.5 - Dreamful Chronicles",
+	"th10": "Touhou Dream Rebellion Episode 6 - Artificial Faith Machine"
 }
---
+++ UltraStory/th10/musiccmt.js
@@ -0,0 +1,182 @@
+{
+    "1": [
+        "@",
+        "　",
+        "This is the title theme.",
+        "",
+        "New game, new ecl format.",
+        "So this is the first game to feature ZUN's new engine,",
+        "and therefore I had to learn alot of new ecl stuff.",
+        "But I managed it in the end."
+    ],
+    "2": [
+        "@",
+        "　",
+        "This is the first stage theme.",
+        "I wanted to go with a winter theme for this game,",
+        "since the last game was set in harvest.",
+        "The thought of changing the season in which the game takes place",
+        "struck me as really fun, and made for alot of pretty scenery.",
+        "Shame I can't change the background fog."
+    ],
+    "3": [
+        "@",
+        "　",
+        "This is Minoriko Aki's theme.",
+        "",
+        "She's back, due to the harvest being way too strong this year.",
+        "But she's still a pushover, especially after getting beaten so much",
+        "in the last game.",
+        ""
+    ],
+    "4": [
+        "@",
+        "　",
+        "This is the second stage theme.",
+        "A forest in winter can be a really pretty time.",
+        "Snow clinging onto the branches, snow on the ground...",
+        "Well, there's just alot of snow I guess.",
+        "But it sparkles when the light shines on it just right!",
+        "That's gotta count for something, right?"
+    ],
+    "5": [
+        "@",
+        "　",
+        "This is Hina Kagiyama's theme.",
+        "",
+        "Hina, with her weird facial expressions!",
+        "Yeah, I got nothin to say here.",
+        "",
+        "Fun fight, though."
+    ],
+    "6": [
+        "@",
+        "　",
+        "This is the third stage theme.",
+        "",
+        "So far, its going very smoothly. Probably also because there's",
+        "just not much dialouge to write. Only two scenarios afterall.",
+        "Also, this theme...still love it so so much!",
+        "Let's rush through the frozen valley!"
+    ],
+    "7": [
+        "@",
+        "　",
+        "This is Nitori Kawashiro's theme.",
+        "Nitori has quite a bit of involvement this time around.",
+        "Well, she's still only a stage 3 boss, but she made something, apparently.",
+        "Just wait and see how the plot evolves in the second half of the game.",
+        "Because this...is the end of the demo!",
+        "I hope its fun so far! ^^"
+    ],
+    "8": [
+        "@",
+        "　",
+        "This is the fourth stage theme.",
+        "When you think of waterfalls, there are two images:",
+        "The powerful, vast waterfall and the meek, trickling one.",
+        "But this time, I opted for the image of the former. Touhou has always, always had a dark feel",
+        "to it, so I tried to express a somewhat refreshing feel. Why is that?",
+        "A falls waterfall. I think the thing waterfalls convey the most is during fall."
+    ],
+    "9": [
+        "@",
+        "　",
+        "This is Aya Shameimaru's theme.",
+        "",
+        "I made the same Aya's theme, Wind God Girl, with the image of Aya the newspaper reporter,",
+        "but this time I emphasized the image of the youkai \"Tengu\" from ancient Japan.",
+        "If I somehow gave it an old, nostalgic feeling, you too would become one of the youkai.",
+        "Ahh, it feels so nostalgic."
+    ],
+    "10": [
+        "@",
+        "　",
+        "This is the fifth stage theme.",
+        "This time, the Japanese element is deep throughout. Especially as it gets more Japanese the",
+        "more you progress through the latter half.",
+        "Comparing this tune to the others, the Japanese element is relatively small, but the very dark,",
+        "nostalgic melody, and rapid chorus feel very latter half. ",
+        "Even so, the Moriya Shrine seems even more extravagant than the Hakurei Shrine..."
+    ],
+    "11": [
+        "@",
+        "　",
+        "This is Sanae Kochiya's theme.",
+        "It feels like the metal section suddenly got bigger here; I thought I'd give this a heavy feeling,",
+        "but I got careless and overdid it, which is a bad habit of mine. Tee hee.",
+        "Whether it's the melody or something else, it's the heaviest in this game.",
+        "From this point on, it suddenly becomes bright, and at the end it kind of lost its touch...",
+        "The stronger they are, the less the feeling of tension: This is the promise in Gensokyo."
+    ],
+    "12": [
+        "@",
+        "　",
+        "This is the sixth stage theme.",
+        "Since the stage is short, the tune is also short. The final stage has a drawn out Last Boss battle,",
+        "so if I didn't make the path leading to it shorter, it would lose its potency, which is why it's",
+        "short. I'm not omitting the vital steps.",
+        "I tried expressing the feeling \"hey, something's about to start\" in this tune.",
+        "Of course something is going to start, since it's the final stage after all."
+    ],
+    "13": [
+        "@",
+        "　",
+        "This is Kanako Yasaka's theme.",
+        "This is a strange god who makes all human and youkai her friends with an overwhelming power.",
+        "I composed a part which makes you feel awe toward a god, and a part which makes you feel",
+        "the gravity of history.",
+        "Anyhow, it's a playful tune which possesses ample reckless abandon. There's a huge difference",
+        "compared to Sanae in the melody, but maybe this is the difference between gods and humans."
+    ],
+    "14": [
+        "@",
+        "　",
+        "This is the extra stage theme.",
+        "I thought of writing the most cheerful piece for this game.",
+        "Not especially tasty yakisoba, okonomiyaki, takoyaki, and heavily seasoned",
+        "gesoyaki. And unusually expensive.",
+        "Served as a junk food snack platter—with cans of beer—all the while gazing at the grounds.",
+        "I love that kind of festival day at the shrine. That's one of the ways to enjoy beer."
+    ],
+    "15": [
+        "@",
+        "　",
+        "This is Suwako Moriya's theme.",
+        "It's a cheerful piece with a rapid feeling. Moreover, I tried to add a Japanese and childish",
+        "feeling to the melody. It fits Suwako perfectly.",
+        "Such a lively tune is on the contrary, actually a piano piece from hell.",
+        "Again, this song has an abundance of chaos, even more so than Kanako. You'd definitely think",
+        "she was playing around with the shrine maiden, what with the particularly high speed piano."
+    ],
+    "16": [
+        "@",
+        "　",
+        "This is the ending theme.",
+        "",
+        "This time, there're many triple beats that permeate your whole body.",
+        "I feel that things like triple beat match the Japanese-style game nicely.",
+        "It asserts itself modestly without being too dark or too light.",
+        "Made this way, it becomes an atmospheric piece, thus it's suitable for any scene."
+    ],
+    "17": [
+        "@",
+        "　",
+        "This is the Staff Credits theme.",
+        "Pray to the gods for rain, and rain will fall. It doesn't have any relevance to the actual story, but",
+        "the simplest thing to understand as a god's blessing is rain, so...",
+        "The wind god doesn't just make the wind blow like a tengu can; there's also a connection",
+        "to the meaning of harvest gods. If you think of farming, you think rain.",
+        "That's why it's rain."
+    ],
+    "18": [
+        "@",
+        "　",
+        "This is the Game Over theme.",
+        "",
+        "It's too lonely when the Game Over screen is silent, so I put this in.",
+        "Oh, how mysterious, it's even more lonely, now.",
+        "",
+        "But why?"
+    ]
+}
