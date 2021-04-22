ccb327e2d0cf9dd698080a6451391397ef1d408d
PKWeegee <pk.weegee@gmx.at>
2021-04-22 22:25:03 +0200

    UltraStory th16 Full

--- UltraStory/th16/musiccmt.js
+++ UltraStory/th16/musiccmt.js
@@ -42,7 +42,7 @@
         "",
         "This is Lunaa Verediem's theme.",
         "Actual Theme: Explosive Ice World Stone ~ Oxygen Conversion",
-        "You may recognize here from somewhere else. I won't tell where though c:",
+        "You may recognize her from somewhere else. I won't tell where though c:",
         "She is very sleep deprived, which may or may not be because of being in the",
         "deep dream realm for such a long time. But then again, time is but an",
         "illusion in this place. So who knows how long it has been?"
@@ -71,97 +71,97 @@
         "@",
         "",
         "This is the Stage 4 theme.",
-        "",
-        "\"Winter\" means \"blizzards,\" that's for sure.",
-        "You might not know this if you don't live in a snowy country, but blizzards are actually different",
-        "from rainstorms. They happen when snow gets whipped up off the ground and blows back down on a",
-        "horizontal slant. Wearing a straw hat in one is pointless."
+        "Actual Theme: Esmeralda Cavern",
+        "It was pretty impossible to remove the fog effect.",
+        "So I just made it a story element, why not.",
+        "A throne room seems like a pretty final-y area,",
+        "but we are here to divert expectations."
     ],
     "9": [
         "@",
         "",
-        "This is Narumi Yatadera's theme.",
-        "",
-        "I composed it with an image of someone with her feet planted firmly on the ground. The original ",
-        "Jizo Bodhisattva is an honorable figure who provides salvation in the long era until Maitreya ",
-        "appears, but at some point or another, a countless number of eccentric Jizo statues spread ",
-        "throughout all of Japan. So I figure it'd be nice if there was a magic Jizo somewhere in there."
+        "This is Mari Ackame's theme.",
+        "Actual Theme: Edge of the Helix ~ Red Marriage",
+        "Mari is a person, who is extremely close to the mastermind.",
+        "A vampire who doesn't like being a vampire, aint that neat?",
+        "She is pretty determined to make sure the mastermind doesn't get harmed. ",
+        "But yet she still wants all this to end, huh?"
     ],
     "10": [
         "@",
         "",
         "This is the Stage 5 theme.",
-        "",
-        "A world that's creepier than anything in Touhou thus far.",
-        "Although, when you consider the fact that all those doors are connected ",
-        "to people's backs, I guess you could call it convenient in a way.",
-        "They seem like they'd be kind of lukewarm, those doors."
+        "Actual Theme: Academia Dance Moon Token",
+        "It was kinda hard to figure out what I wanted the doors to be.",
+        "In the end, I crammed in some refrences, and some cool places. ",
+        "Maybe you recognize some of them?",
+        ""
     ],
     "11": [
         "@",
         "",
-        "This is Mai Teireida and Satono Nishida's theme.",
-        "",
-        "I incorporated a great deal of things into this song: eeriness, lightheartedness,",
-        "strong hostility, innocence, and on top of all that, a midboss-esque feeling.",
-        "I put enough in for both characters, so it's an extravagant song in a number of ways. ",
-        "Created with an image of them dancing madly and eerily."
+        "This is Seraph and Aternia's theme.",
+        "Actual Theme: Sound of a Nightmare",
+        "A deadly duo, if you cross them at the wrong time.",
+        "Be thankful that they are on your side (?) right now.",
+        "I love the feeling this theme gives off, ",
+        "it fits so perfectly."
     ],
     "12": [
         "@",
         "",
         "This is the Stage 6 theme.",
-        "",
-        "Made with an image of a high-speed stage before the boss.",
-        "I think it exudes a feeling of being rushed along, ",
-        "regardless of the nature of the story going on."
+        "Actual Theme: Life-Form Inspectors Historical Records",
+        "One of the most beautiful backgrounds I made I think.",
+        "And the music is the perfect touch as well,",
+        "giving a sense of mystery."
     ],
     "13": [
         "@",
         "",
-        "This is Okina Matara's theme.",
-        "",
-        "A very \"This is the climax!\" sort of song with a lot of strength to it.",
-        "It has a strong melody, even compared to the other songs in the game, so it might've",
-        "been nice if I just went all-in and started singing lyrics to it. It's very likely that ",
-        "you'd start laughing too hard to even play the game, but that might actually be the best outcome."
+        "This is Jessica Puella Kaisa's theme.",
+        "Actual Theme: Being of Discord ~ the World Barrier",
+        "This theme encapsulates her internal struggle and madness.",
+        "She certainly isn't herself in this fight,",
+        "and if you play on higher difficulties, ",
+        "you might be able to spot some extra lore."
     ],
     "14": [
         "@",
         "",
-        "This is the Extra Stage theme.",
-        "",
-        "A mysterious world that we dive into once more, having mustered up our resolve.",
-        "It's a song that combines both eeriness and gentleness.",
-        "",
-        "The song does a good job of conveying the stage's difficulty, so it's pretty depressing. (lol)"
+        "This is the Nightmare Stage theme.",
+        "Actual Theme: The Dream where Regrets are Carried, Even in Afterlife",
+        "I chose to release extra alongside the maingame this time.",
+        "Because of the way this game works, there'd be no suspense",
+        "regarding who the extra boss would be.",
+        "So yeah, sorry for the longer wait, heh."
     ],
     "15": [
         "@",
         "",
-        "The theme for Okina Matara (True).",
-        "",
-        "Even though it's an Extra Stage, it wound up being part of the main story.",
-        "Since it's your true battle with Okina, I swept away all the refreshing feeling of nature,",
-        "but still made it a song with a strong melody befitting of the true final boss.",
-        "Honestly, getting attacked from behind is just the worst!"
+        "The theme for Jessica Puella Kaisa (Nightmare Stage).",
+        "Actual Theme: The Girl who dreamt of Happiness ~ Dream Rebellion",
+        "A very emotional fight, in my opinion.",
+        "A girl, at the brink of madness, who had to come to terms with her mistakes.",
+        "Quite a tragic end for this story.",
+        "But is it...?"
     ],
     "16": [
         "@",
         "",
         "This is the ending theme.",
-        "",
-        "It kind of feels like a bad ending, but to tell the truth,",
-        "all the endings this time are like bad endings."
+        "Actual Theme: Leaves of Ancient Words",
+        "Thank you all so much for sticking around,",
+        "this has been an incredibly wild ride."
     ],
     "17": [
         "@",
         "",
         "This is the staff roll theme.",
-        "",
-        "It's a different version of the winter stage theme made with an image of a refreshing conclusion.",
-        "",
-        "Speaking of which. Why is a \"winter blizzard\" song refreshing, you ask?",
-        "That's because I made it with \"early morning\" in mind. <i$Fuyu wa tsutomete>, as they say."
+        "Actual Theme: Longevity of the Yellow River ~ Ancient Dreams...",
+        "Shoutouts to Treasure Castle Labyrinth, its ost is absolutely amazing.",
+        "It gave me so much inspiration, and really formed this game into what it is.",
+        "Be sure to check out the fangame of TCL, I'll throw a link in the credits txt file.",
+        "And with that...see you next time!"
     ]
 }
