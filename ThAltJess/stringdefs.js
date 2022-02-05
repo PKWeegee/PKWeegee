4c548caf05d597fb61bca6c2e8734b6a21cb1c21
PKWeegee <pk.weegee@gmx.at>
2022-02-05 20:21:42 +0100

    thaltjess th08 demo

--- ThAltJess/stringdefs.js
+++ ThAltJess/stringdefs.js
@@ -1,4 +1,5 @@
 {
 	"th06": "Touhou 06 - Embodiment of Scarlet Devil: Arrival",
-	"th07": "Touhou 07 - Perfect Cherry Blossom: Home"
+	"th07": "Touhou 07 - Perfect Cherry Blossom: Home",
+	"th08": "Touhou 08 - Imperishable Night: Power"
 }
